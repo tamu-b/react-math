b1fec079e7c99072708381762cbd34b7d977bcf1
tamu-b <tamurahideaki.b@gmail.com>
2026-06-03 17:26:25 +0900

    Fractionにmarginを設定

--- src/components/Fraction/index.tsx
+++ src/components/Fraction/index.tsx
@@ -9,9 +9,9 @@ type Props = {
 export const Fraction: FC<Props> = ({ numerator, denominator }) => {
   return (
     <div className={styles.fraction}>
-      <div>{numerator}</div>
+      <div className={styles.numerator}>{numerator}</div>
       <div className={styles.bar} />
-      <div>{denominator}</div>
+      <div className={styles.denominator}>{denominator}</div>
     </div>
   )
 }
