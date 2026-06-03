89510d5b9dc65c342ca7f26ecabf136f1e31523e
tamu-b <tamurahideaki.b@gmail.com>
2026-06-03 18:52:47 +0900

    Exponentの修正
    base, exponentの型をReactNodeに修正
    スタイルの修正

--- src/components/Exponent/index.stories.tsx
+++ src/components/Exponent/index.stories.tsx
@@ -16,3 +16,17 @@ export const Default: Story = {
     exponent: 3,
   },
 }
+
+export const NestedExponent: Story = {
+  args: {
+    base: 2,
+    exponent: <Exponent base={3} exponent={4} />,
+  },
+}
+
+export const BaseHasExponent: Story = {
+  args: {
+    base: <Exponent base={2} exponent={3} />,
+    exponent: 4,
+  },
+}
--- src/components/Exponent/index.tsx
+++ src/components/Exponent/index.tsx
@@ -1,16 +1,29 @@
-import type { FC } from 'react'
+import { useLayoutEffect, useRef, type FC, type ReactNode } from 'react'
 import styles from './index.module.css'
 
 type Props = {
-  base: number
-  exponent: number
+  base: ReactNode
+  exponent: ReactNode
 }
 
 export const Exponent: FC<Props> = ({ base, exponent }) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const exponentRef = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    const el = exponentRef.current
+    const container = containerRef.current
+    const shift = el.getBoundingClientRect().height * 0.4
+    container.style.paddingTop = `${shift}px`
+    el.style.transform = `translateY(${-shift}px)`
+  }, [])
+
   return (
-    <div className={styles.exponent}>
+    <div ref={containerRef} className={styles.exponent}>
       <div>{base}</div>
-      <div className={styles.sup}>{exponent}</div>
+      <div ref={exponentRef} className={styles.sup}>
+        {exponent}
+      </div>
     </div>
   )
 }
